76709214963e4d0a31471f3da8eda81cd0c20032
kimhwan <kimhwan@kimhwanui-MacBookPro.local>
2021-03-30 23:11:14 +0900

    delete customers controller, delete ERD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
-import { CustomersController } from './customers/customers.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController, CustomersController],
+  controllers: [AppController, CatsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/customers/customers.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('customers')
-export class CustomersController {
-  @Get('profile')
-  getProfile(): string {
-    return 'get profile';
-  }
-}
